1de78b33079f5b577ceda2c84927b4213821eea5
raloliver <contato@raloliver.com.br>
2021-10-07 14:25:43 +0100

    22: add DTO

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,11 +2,12 @@
  * File: tasks.controller.ts
  * Project: nestjs-tasks
  * Created: Friday, September 3rd 2021, 6:43:59 am
- * Last Modified: Thursday, October 7th 2021, 8:23:02 am
+ * Last Modified: Thursday, October 7th 2021, 2:23:27 pm
  * Copyright © 2021 AMDE Agência
  */
 
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AddTaskDto } from './dto/add-task-dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -25,12 +26,11 @@ export class TasksController {
    * Example: @Body() body.
    *
    * If you wanna share pick just some attributes, just follow the example below.
+   * @Body('title') title: string,
+   * @Body('description') description: string,
    */
   @Post()
-  public addTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Task {
-    return this.taskService.addTask(title, description);
+  public addTask(@Body() addTaskDto: AddTaskDto): Task {
+    return this.taskService.addTask(addTaskDto);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,13 +2,14 @@
  * File: tasks.service.ts
  * Project: nestjs-tasks
  * Created: Friday, September 3rd 2021, 6:56:13 am
- * Last Modified: Thursday, October 7th 2021, 6:35:14 am
+ * Last Modified: Thursday, October 7th 2021, 2:24:30 pm
  * Copyright © 2021 AMDE Agência
  */
 
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
+import { AddTaskDto } from './dto/add-task-dto';
 
 @Injectable()
 export class TasksService {
@@ -18,7 +19,9 @@ export class TasksService {
     return this.tasks;
   }
 
-  public addTask(title: string, description: string): Task {
+  public addTask(addTaskDto: AddTaskDto): Task {
+    const { title, description } = addTaskDto;
+
     const task: Task = {
       id: uuid(),
       title,
