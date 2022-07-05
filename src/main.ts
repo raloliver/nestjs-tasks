27b0f75c2548e33c151dc22eeaa2b67d2aa0c989
raloliver <contato@raloliver.com.br>
2022-07-05 06:46:58 +0100

    ValidationPipe: Creating a Task

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@
  * File: main.ts
  * Project: nestjs-tasks
  * Created: Thursday, September 2nd 2021, 8:43:02 pm
- * Last Modified: Friday, September 3rd 2021, 6:30:59 am
+ * Last Modified: Tuesday, July 5th 2022, 6:45:13 am
  * Copyright © 2021 AMDE Agência
  */
 
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
--- src/tasks/dto/add-task-dto.ts
+++ src/tasks/dto/add-task-dto.ts
@@ -2,11 +2,16 @@
  * File: add-task-dto.ts
  * Project: nestjs-tasks
  * Created: Thursday, October 7th 2021, 2:20:33 pm
- * Last Modified: Thursday, October 7th 2021, 2:21:44 pm
+ * Last Modified: Tuesday, July 5th 2022, 6:42:04 am
  * Copyright © 2021 AMDE Agência
  */
 
+import { IsNotEmpty } from 'class-validator';
+
 export class AddTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
