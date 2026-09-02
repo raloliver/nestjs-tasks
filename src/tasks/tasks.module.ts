ead299e9af6301735c364b81ab9f78c94909f669
Israel Oliveira <raloliver.ws@gmail.com>
2026-09-02 11:43:07 -0300

    Feature/orm (#6)
    * Create an Entity for Tasks
    
    * feat: update packages
    
    * fix: include build info on gitignore
    
    * Creating a Tasks Repository
    
    * Init persistence getting a task by id
    
    * Persistence Creating a Task
    
    * Persistence Deletion a Task
    
    * Persistence Update Task Status
    
    * Persistance Getting All Tasks
    
    ---------
    
    Co-authored-by: raloliver <contato@raloliver.com.br>

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,16 +2,21 @@
  * File: tasks.module.ts
  * Project: nestjs-tasks
  * Created: Friday, September 3rd 2021, 6:30:34 am
- * Last Modified: Friday, September 3rd 2021, 6:45:25 am
+ * Last Modified: Tuesday, September 1st 2026, 6:41:40 pm
  * Copyright © 2021 AMDE Agência
  */
 
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TasksRepository } from './tasks.repository';
+import { Task } from './task.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, TasksRepository],
 })
 export class TasksModule {}
